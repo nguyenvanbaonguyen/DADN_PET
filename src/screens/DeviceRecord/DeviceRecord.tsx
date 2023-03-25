307e9aef3511cf30345cf2d6458d50fc45d2a89b
nhiepchau <nhiepbaochau@gmail.com>
2023-03-26 04:09:51 +0700

    device record

--- src/screens/DeviceRecord/DeviceRecord.tsx
+++ src/screens/DeviceRecord/DeviceRecord.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import ButtonCustom from '~/components/ButtonCustom/ButtonCustom';
+import Pagination from '~/components/Pagination/Pagination';
+import TextCustom from '~/components/TextCustom/TextCustom';
+import ViewCustom from '~/components/ViewCustom/ViewCustom';
+import HistoryDay from './components/HistoryDay';
 
 const DeviceRecord = () => {
-  return <Text>DeviceRecord</Text>;
+  return (
+    <ScrollView>
+      <ViewCustom layout="px-15 mt-15 pb-30">
+        <ViewCustom layout="fd-r jc-b">
+          <TextCustom type="fs-20 fw-semi">Lịch sử hoạt động</TextCustom>
+          <ViewCustom layout="h-30 w-30 jc-c ai-c">
+            <Icon color={'black'} size={25} name="filter" />
+          </ViewCustom>
+        </ViewCustom>
+        <HistoryDay />
+        <HistoryDay />
+        <HistoryDay />
+        <HistoryDay />
+        <ViewCustom layout="ai-e mt-10">
+          <ButtonCustom size="xs" content="Download" layout="mt-15" />
+        </ViewCustom>
+        <ViewCustom layout="ai-c">
+          <Pagination />
+        </ViewCustom>
+      </ViewCustom>
+    </ScrollView>
+  );
 };
 
 export default DeviceRecord;
--- src/screens/HistoryUser/HistoryUser.tsx
+++ src/screens/HistoryUser/HistoryUser.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import ButtonCustom from '~/components/ButtonCustom/ButtonCustom';
 import Pagination from '~/components/Pagination/Pagination';
 import TextCustom from '~/components/TextCustom/TextCustom';
 import ViewCustom from '~/components/ViewCustom/ViewCustom';
@@ -9,12 +11,20 @@ const HistoryUser = () => {
   return (
     <ScrollView>
       <ViewCustom layout="px-15 mt-15 pb-30">
-        <TextCustom type="fs-20 fw-semi">Lịch sử hoạt động</TextCustom>
+        <ViewCustom layout="fd-r jc-b">
+          <TextCustom type="fs-20 fw-semi">Lịch sử hoạt động</TextCustom>
+          <ViewCustom layout="h-30 w-30 jc-c ai-c">
+            <Icon color={'black'} size={25} name="filter" />
+          </ViewCustom>
+        </ViewCustom>
         <HistoryDay />
         <HistoryDay />
         <HistoryDay />
         <HistoryDay />
         <ViewCustom layout="ai-e mt-10">
+          <ButtonCustom size="xs" content="Download" layout="mt-15" />
+        </ViewCustom>
+        <ViewCustom layout="ai-c">
           <Pagination />
         </ViewCustom>
       </ViewCustom>
